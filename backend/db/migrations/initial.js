01776a2a4daa516ea0fa0e20a74821b562e83798
gavin-love <1co.loco@oprotonmail.ch>
2018-07-22 15:36:21 -0600

    Create backend

---
+++ backend/db/migrations/initial.js
@@ -0,0 +1,20 @@
+
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('users', function (table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('email');
+      table.string('password');
+
+      table.timestamps(true, true);
+    }),
+  ])
+};
+
+
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('users'),
+  ]);
+};
