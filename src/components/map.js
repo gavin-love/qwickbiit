cf93111f85850c45429c5f998a1b14e9f7ac5da9
gavin-love <1co.loco@oprotonmail.ch>
2018-07-22 11:10:11 -0600

    Revert "Add geolocation to map"
    This reverts commit c63fde619cc1437da0e95f426cee82b7c27d1a1a.

--- src/components/map.js
+++ src/components/map.js
@@ -5,46 +5,35 @@ export class MapContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedPlaces: [],
+      selectedPlace: { name: 'hello' }
     }
-
   }
 
-  // nearbyPlaces = (props, map, clickEvent) => {
-  //   const { google } = props
-  //   const service = new google.maps.places.PlacesService(map);
+  mapClicked(mapProps, map, clickEvent) {
+    const { google } = mapProps
+    const service = new google.maps.places.PlacesService(map);
 
-  //   service.nearbySearch({
-  //     location: this.props.position,
-  //     radius: 500,
-  //     keyword: ['food']
-  //   }, result => this.setState({ selectedPlaces: result })
-  //   )
-  // }      switch this method to get nearby places from yelp then render markers with yelp lat and lng!!!!
-
-
-
+    service.nearbySearch({
+      location: { lat: 39.7392, lng: -104.9903 },
+      radius: 50,
+      keyword: ['restaurants']
+    }, result => console.log(result))
+  }
 
   render() {
-    const location = this.props.position;
-    // const markers = this.state.selectedPlaces.map(restaurant => {
-    //   console.log(restaurant)
-    //   return (
-    //     <Marker
-    //       position={{ lat: 80, lng: -141 }}
-    //     />
-    //   )
-    // })
+    const location = {
+      lat: 39.7392,
+      lng: -104.9903
+    }
 
     return (
       <div>
-        <Map
-          google={this.props.google}
+        <Map google={this.props.google}
           initialCenter={location}
-          onReady={this.nearbyPlaces}
-        >
-          <Marker position={this.props.position} />
-        </Map>
+          onClick={this.mapClicked} />
+
+        <Marker onClick={this.onMarkerClick}
+          name={'Current location'} />
       </div>
     )
   }
