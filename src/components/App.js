4218f15859cba1dc10cd827f3b2f22682b1e39aa
gavin-love <1co.loco@oprotonmail.ch>
2018-07-22 13:59:20 -0600

    Add yelp api fetch call

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,50 @@ import './App.css';
 import Footer from '../components/mainFooter';
 import MapContainer from '../components/map';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      currentPosition: {}
+    })
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  getLocation = () => {
+
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 8000,
+      maximumAge: 0
+    };
+
+    const success = (pos) => {
+      let position = pos.coords;
+
+      let currentLocation = {
+        lat: position.latitude,
+        lng: position.longitude
+      }
+
+      this.setState({
+        currentPosition: currentLocation
+      })
+    };
+
+    const error = (err) => console.warn(`ERROR(${err.code}): ${err.message}`);
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <MapContainer />
+        <MapContainer position={this.state.currentPosition} />
         <Footer />
       </div>
     );
--- src/components/map.js
+++ src/components/map.js
@@ -5,22 +5,67 @@ export class MapContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedPlace: { name: 'hello' }
+      nearbyPlaces: []
     }
   }
 
-  mapClicked(mapProps, map, clickEvent) {
-    const { google } = mapProps
-    const service = new google.maps.places.PlacesService(map);
+  nearbyPlaces = async (
+    // latitude,
+    // longitude,
+  ) => {
 
-    service.nearbySearch({
-      location: { lat: 39.7392, lng: -104.9903 },
-      radius: 50,
-      keyword: ['restaurants']
-    }, result => console.log(result))
-  }
+    let restaurants;
+
+    let yelpApiKey = 'Dy1whqeBla6eVA7q4irOM-bi8QNVgda5nwcAuhJHkSdhJogs1AqY_Qgs78YrfJITrQ99tbzSQpLZ7kT6ndXN_Iw4-Q-oKr9dxXqC2ihe7cqWwDAsWbY_yGxbixdRW3Yx'
+
+    const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
+
+    const prefix = `${corsAnywhereUrl}https://api.yelp.com/v3/businesses/search?`;
+
+    const lat = `latitude=39.7392`;
+    const limit = 'limit=5';
+    const term = 'term=restaurants';
+    const long = `longitude=-104.9903`;
+    const price = 'price=2'
+    const radius = `radius=1609`;
+    const sort = 'sort_by=rating';
+    const open = 'open_now=true';
+    const reservation = 'attributes=reservation'
+
+    let headers = new Headers();
+    headers.append("Authorization", "Bearer " + yelpApiKey);
+
+    try {
+      const result =
+        await fetch(`${prefix}${lat}&${long}&${term}&${price}&${limit}&${radius}&${open}&${reservation}&${sort}`,
+          {
+            headers
+          });
+
+      const data = await result.json();
+      restaurants = data.businesses;
+
+      console.log(restaurants)
+      // return suggestedRestaurantsCleaner(restaurants);
+    } catch (error) {
+      return [];
+    }
+  };
+
+  // nearbyPlaces(mapProps, map, clickEvent) {
+  //   const { google } = mapProps
+  //   const service = new google.maps.places.PlacesService(map);
+
+  //   service.nearbySearch({
+  //     location: { lat: 39.7392, lng: -104.9903 },
+  //     radius: 50,
+  //     keyword: ['restaurants']
+  //   }, result => this.setState({ nearbyPlaces: result })
+  //   )
+  // }
 
   render() {
+    // const location = this.props.position;
     const location = {
       lat: 39.7392,
       lng: -104.9903
@@ -30,10 +75,13 @@ export class MapContainer extends Component {
       <div>
         <Map google={this.props.google}
           initialCenter={location}
-          onClick={this.mapClicked} />
-
-        <Marker onClick={this.onMarkerClick}
-          name={'Current location'} />
+          onReady={this.nearbyPlaces}
+        >
+          <Marker
+            initialCenter={location}
+          // onReady={this.nearbyMarkers}
+          />
+        </Map>
       </div>
     )
   }
