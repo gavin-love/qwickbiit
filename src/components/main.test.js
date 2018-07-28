84b1b910bdff96ef1e6b3705fb860d6069fd066a
gavin-love <1co.loco@oprotonmail.ch>
2018-07-27 18:43:15 -0600

    Add map restaurants to state test

--- src/components/main.test.js
+++ src/components/main.test.js
@@ -1,7 +1,15 @@
 import { mapDispatchToProps, mapStateToProps } from './main';
 
 describe('mapState', () => {
+
   it('should map location to state', () => {
+    const mockState = {}
+    const expected = {}
+    const mappedProps = mapStateToProps(mockState);
+    expect(mappedProps).toEqual(expected);
+  })
+
+  it('should map restaurants to state', () => {
     const mockState = {}
     const expected = {}
     const mappedProps = mapStateToProps(mockState);
