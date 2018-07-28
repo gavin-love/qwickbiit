32a4924f951ca828ae501c6f48c5e3fca6947047
gavin-love <1co.loco@oprotonmail.ch>
2018-07-27 18:41:33 -0600

    Add map location to props test

---
+++ src/components/main.test.js
@@ -0,0 +1,10 @@
+import { mapDispatchToProps, mapStateToProps } from './main';
+
+describe('mapState', () => {
+  it('should map location to state', () => {
+    const mockState = {}
+    const expected = {}
+    const mappedProps = mapStateToProps(mockState);
+    expect(mappedProps).toEqual(expected);
+  })
+})
