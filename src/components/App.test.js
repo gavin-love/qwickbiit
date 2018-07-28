8589ce2a3194062656e6091ba45f4ae41f82c3ac
gavin-love <1co.loco@oprotonmail.ch>
2018-07-27 18:10:11 -0600

    Add map location to state test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mapDispatchToProps, mapStateToProps } from "./App";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('mapState', () => {
+  it('should map location to state', () => {
+    const mockState = {}
+    const expected = {}
+    const mappedProps = mapStateToProps(mockState);
+    expect(mappedProps).toEqual(expected);
+  })
+})
